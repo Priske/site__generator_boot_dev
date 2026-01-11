00761c6b874d42c1722ff3d7b80e6da1cb13c61f
Priske <eeckman_ben@hotmail.com>
2026-01-11 13:31:27 +0100

    added pages

--- src/main.py
+++ src/main.py
@@ -4,11 +4,12 @@ from gencontent import extract_title,generate_page
 
 
 def main():
-    #remove_dir("./public")
-    #copy_dir("./static","./public")
+
     generate_page("content/index.md",
                   "template.html",
                   "public/index.html")
+    
+
 
 main()
 
